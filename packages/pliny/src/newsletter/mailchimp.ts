01fe2538b9391a6cf778d32864bc78effbb28bef
Dan Mindru <mindrudan@gmail.com>
2025-01-24 12:10:28 +0100

    Update deps and remove old mailchimp dep

--- packages/pliny/src/newsletter/mailchimp.ts
+++ packages/pliny/src/newsletter/mailchimp.ts
@@ -1,14 +1,26 @@
-import mailchimp from '@mailchimp/mailchimp_marketing'
-
-mailchimp.setConfig({
-  apiKey: process.env.MAILCHIMP_API_KEY,
-  server: process.env.MAILCHIMP_API_SERVER, // E.g. us1
-})
-
 export const mailchimpSubscribe = async (email: string) => {
-  const response = await mailchimp.lists.addListMember(process.env.MAILCHIMP_AUDIENCE_ID, {
-    email_address: email,
-    status: 'subscribed',
-  })
-  return response
+  const apiKey = process.env.MAILCHIMP_API_KEY
+  const server = process.env.MAILCHIMP_API_SERVER
+  const audienceId = process.env.MAILCHIMP_AUDIENCE_ID
+
+  const response = await fetch(
+    `https://${server}.api.mailchimp.com/3.0/lists/${audienceId}/members`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `apikey ${apiKey}`,
+      },
+      body: JSON.stringify({
+        email_address: email,
+        status: 'subscribed',
+      }),
+    }
+  )
+
+  if (!response.ok) {
+    throw new Error(`Error: ${response.statusText}`)
+  }
+
+  return await response
 }
